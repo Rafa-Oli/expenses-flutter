cc9726f9f62865349a3e695e5207ace03efb8749
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-24 14:14:08 -0300

    flutter

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 430,
       child: transactions.isEmpty
           ? Column(children: <Widget>[
               SizedBox(height: 20),
