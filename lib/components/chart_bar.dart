a3d972579b2cdfc78864d90d632cbf8fd1736533
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-26 19:48:55 -0300

    flutter

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -15,46 +15,50 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(
-        height: 20,
-        child: FittedBox(
-          // o fittedBox vai fazer diminuir o valor(se for grande) para caber no espaço ideal
-          child: Text('${value.toStringAsFixed(2)}'),
-        ),
-      ),
-      SizedBox(height: 5),
-      Container(
-        height: 60,
-        width: 10,
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: <Widget>[
-            Container(
-              // estrutura da barra
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                ),
-                color: Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(5),
-              ),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(children: <Widget>[
+          Container(
+            height: 20,
+            child: FittedBox(
+              // o fittedBox vai fazer diminuir o valor(se for grande) para caber no espaço ideal
+              child: Text('${value.toStringAsFixed(2)}'),
             ),
-            FractionallySizedBox(
-              heightFactor: percentage,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(5),
+          ),
+          SizedBox(height: 5),
+          Container(
+            height: constraints.maxHeight *0.8,
+            width: 10,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[
+                Container(
+                  // estrutura da barra
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
-      SizedBox(height: 5),
-      Text(label),
-    ]);
+                FractionallySizedBox(
+                  heightFactor: percentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(label),
+        ]);
+      },
+    );
   }
 }
